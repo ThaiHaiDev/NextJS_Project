541922bc893ec9790b47f0fe11f74a7f0c286279
ThaiHaiDev <thaihai1004@gmail.com>
2023-09-02 16:48:08 +0700

    created api basic and setup http-proxy for path

---
+++ pages/api/[...path].ts
@@ -0,0 +1,25 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import httpProxy from 'http-proxy';
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const proxy = httpProxy.createProxyServer();
+
+// Customer config when send data, no parser data json -> send data direct to server 
+export const config = {
+  api: {
+    bodyParser: false
+  }
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  // don't send cookies to API server
+  req.headers.cookie = '';
+  proxy.web(req, res, {
+    target: process.env.API_URL,
+    changeOrigin: true,
+    selfHandleResponse: false
+  })
+}
