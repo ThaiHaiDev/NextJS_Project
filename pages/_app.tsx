92da6fa1e9d06cb523730b1ee8368360bf658092
ThaiHaiDev <thaihai1004@gmail.com>
2023-09-17 22:38:05 +0700

    setup mui v5 for project next

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,25 @@
 import { SWRConfig } from 'swr'
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 import axiosClient from '@/api/axiosClient'
+import { CacheProvider } from '@emotion/react'
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
+import { AppPropsWithLayout } from '../models';
+import { createEmotionCache, theme } from '@/utils/index';
 
-function MyApp({ Component, pageProps }: AppProps) {
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache()
+
+function MyApp({ Component, pageProps, emotionCache = clientSideEmotionCache }: AppPropsWithLayout) {
   return (
-    <SWRConfig value={{ fetcher: (url) => axiosClient.get(url), shouldRetryOnError: false }}>
-      <Component {...pageProps} />
-    </SWRConfig>
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <SWRConfig value={{ fetcher: (url) => axiosClient.get(url), shouldRetryOnError: false }}>
+          <Component {...pageProps} />
+        </SWRConfig>
+      </ThemeProvider>
+    </CacheProvider>
   )
 }
 
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,19 +1,14 @@
 import * as React from 'react';
 
 import {AdminLayout, MainLayout} from '@/components/layout';
+import { Typography } from '@mui/material';
 
 export interface IAboutPageProps {
 }
 export default function AboutPage (props: IAboutPageProps) {
   return (
     <AdminLayout>
-      <h1>About Page</h1>
-
-      <ul>
-        <li>He</li>
-        <li>Ho</li>
-        <li>Ha</li>
-      </ul>
+      <Typography component="h1" variant='h3' color="primary.main">About Page</Typography>
     </AdminLayout>
   );
 }
