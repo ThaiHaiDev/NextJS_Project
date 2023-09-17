e319a4ca4b03ba38b150f6cded5b26990a4ed321
ThaiHaiDev <thaihai1004@gmail.com>
2023-09-17 16:03:37 +0700

    setup management state swr

---
+++ components/swr/StudentDetail.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import useSWR from 'swr';
+
+
+export interface IStudentDetailProps {
+    studentId: string;
+}
+
+export default function StudentDetail ({ studentId }: IStudentDetailProps) {
+  const { data, error, mutate, isValidating } = useSWR(`/students/${studentId}`, {
+    revalidateOnFocus: false // change tab and back click tab -> not call api
+  });
+
+  const handleMutate = () => {
+    // mutate(value new, recall api)
+    mutate({ name: 'Change data' }, true)
+  }
+
+  return (
+    <div>
+      Name: {data?.name || '--'}
+
+      <button onClick={handleMutate}>Mutate</button>
+    </div>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
+import { SWRConfig } from 'swr'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import axiosClient from '@/api/axiosClient'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SWRConfig value={{ fetcher: (url) => axiosClient.get(url), shouldRetryOnError: false }}>
+      <Component {...pageProps} />
+    </SWRConfig>
+  )
 }
 
 export default MyApp
